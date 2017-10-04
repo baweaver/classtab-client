548eab935c4bab00d76e303012896e66a687115a
Brandon Weaver <keystonelemur@gmail.com>
2017-10-04 00:36:13 -0700

    fix mirage

--- mirage/models/composer.js
+++ mirage/models/composer.js
@@ -1,9 +1,6 @@
-import { Model } from 'ember-cli-mirage';
+import { Model, hasMany } from 'ember-cli-mirage';
 
 export default Model.extend({
-  name: DS.attr(),
-  birthYear: DS.attr('number'),
-  deathYear: DS.attr('number'),
-  tabs: DS.hasMany('tab')
+  tabs: hasMany('tab')
 });
 
--- mirage/models/tab.js
+++ mirage/models/tab.js
@@ -1,14 +1,5 @@
-import { Model } from 'ember-cli-mirage';
+import { Model, belongsTo } from 'ember-cli-mirage';
 
 export default Model.extend({
-  name: DS.attr(),
-  difficulty: DS.attr(),
-  tabFile: DS.attr(),
-  midis: DS.attr(),
-  videos: DS.attr(),
-  tuning: DS.attr(),
-  timeSignature: DS.attr(),
-  tempo: DS.attr(),
-  key: DS.attr(),
-  composer: DS.belongsTo('composer')
+  composer: belongsTo('composer')
 });
